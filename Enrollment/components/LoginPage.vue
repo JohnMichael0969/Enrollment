36189772ba7aca93539dffea999165db93fece6c
JohnMichael0969 <113489915+JohnMichael0969@users.noreply.github.com>
2022-10-13 15:45:52 +0800

    LadongaJohnmIchael Enrollment page

---
+++ Enrollment/components/LoginPage.vue
@@ -0,0 +1,27 @@
+<template>
+<body style="background-color : powderblue;">
+    <div>
+        <h1 style="text-align : center; ">Log In</h1><br>
+        <br>
+    <div class="d-flex justify-content-center  d-flex align-items-center">
+        <br>
+        <br>
+             <form>
+                 <label for="Username">Username:</label><br>
+                  <input id="Username" type="text" >
+                  <br>
+                   <label for="Password" class="form-label">Password</label>
+                    <br>
+                  <input id="Password" type="Password" ><br><br>
+            
+           
+                   <button type="submit" class="btn btn-primary">Submit</button>
+
+             </form>
+
+    </div>
+    </div>
+</body> 
+
+</template>
+
---
+++ Enrollment/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <LoginPage />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
