30ca037382853ff7a80061ea78c6f22ff2107b9e
JohnMichael0969 <113489915+JohnMichael0969@users.noreply.github.com>
2022-10-17 15:11:51 +0800

    Ernrollment page Ladonga

--- Enrollment/components/LoginPage.vue
+++ Enrollment/components/LoginPage.vue
@@ -1,27 +1,80 @@
 <template>
-<body style="background-color : powderblue;">
-    <div>
-        <h1 style="text-align : center; ">Log In</h1><br>
-        <br>
-    <div class="d-flex justify-content-center  d-flex align-items-center">
-        <br>
-        <br>
-             <form>
-                 <label for="Username">Username:</label><br>
-                  <input id="Username" type="text" >
-                  <br>
-                   <label for="Password" class="form-label">Password</label>
-                    <br>
-                  <input id="Password" type="Password" ><br><br>
+<div class="container-fluid">
+    <div class="row no-gutter">
+        <!-- The image half -->
+        <div class="col-md-6 d-none d-md-flex bg-image"></div>
+        <!-- The content half -->
+        <div class="col-md-6 bg-light">
+          <div class="login d-flex align-items-center py-5">
             
-           
-                   <button type="submit" class="btn btn-primary">Submit</button>
 
-             </form>
+    <b-form @submit="onSubmit">
+      <b-form-group
+        id="email"
+        label="Email address:"
+        label-for="input-email"
+        description="We'll never share your email with anyone else.">
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required>
+          </b-form-input>
+      </b-form-group>
 
+      <b-form-group 
+        id="input-password" 
+        label="Password:" 
+        label-for="input-2">
+        <b-form-input
+          id="input-password"
+          v-model="form.password"
+          placeholder="Enter Password"
+          type="password"
+          required>
+        </b-form-input>
+      </b-form-group>
+<router-link
+to="/home">
+<b-button type="submit" class="mr-4" variant="primary" >Submit</b-button>
+</router-link>
+      
+      
+       </b-form>
+   
+          </div>
+        </div>
     </div>
-    </div>
-</body> 
-
+</div>
 </template>
 
+
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          password: '',
+         
+        }
+      }
+    }
+  }
+
+</script>
+
+<style>
+.login,
+.image {
+  min-height: 100vh;
+}
+
+.bg-image {
+  background-image: url('https://therichpost.com/wp-content/uploads/2021/02/login-split.jpg');
+  background-size: cover;
+  background-position: center center;
+}
+</style>
